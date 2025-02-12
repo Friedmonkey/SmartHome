9defce634230303e5fe9b519e2abe1ff1ce75dec
Rik Dijkslag <rikdijkslag@gmail.com>
2025-02-12 14:34:18 +0100

    Api Context

--- SmartHome.Backend/Api/PersonTestingService.cs
+++ SmartHome.Backend/Api/PersonTestingService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using SmartHome.Common.Api;
 using SmartHome.Common.Models;
+using SmartHome.Database;
 using static SmartHome.Common.Api.IPersonTestingService;
 
 namespace SmartHome.Backend.Api;
@@ -9,11 +10,19 @@ public class PersonTestingService : IPersonTestingService
 {
     private static List<Person> _persons = new();
 
+    private readonly ApiContext _context;
+
+    public PersonTestingService(ApiContext context)
+    {
+        _context = context;
+    }
+
     public async Task<SuccessResponse> AddPerson(AddPersonRequest request)
     {
         if (request.person is null)
             return SuccessResponse.Failed("request person object was null");
         await Task.Delay(500);
+        _context.Devices.ToList();
         _persons.Add(request.person);
         return SuccessResponse.Success();
     }
