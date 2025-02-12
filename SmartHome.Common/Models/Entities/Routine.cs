a48b189c0da5581c2a977507872f44495b9d033d
FriedMonkey <120042050+Friedmonkey@users.noreply.github.com>
2025-02-12 13:59:31 +0100

    added entities

---
+++ SmartHome.Common/Models/Entities/Routine.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SmartHome.Common.Models.Entities;
+public class Routine : Entity
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column(TypeName = "varchar(60)")]
+    public string Id { get; set; }
+
+    [Required]
+    [Column(TypeName = "varchar(20)")]
+    public string Name { get; set; }
+
+    [Required]
+    [Column(TypeName = "datetime(6)")]
+    public DateTime Start { get; set; }
+
+    [Required]
+    [Column(TypeName = "varchar(60)")]
+    public string SmartHomeId { get; set; }
+
+    [Required]
+    [Column(TypeName = "binary(7)")]
+    public byte RepeatDays { get; set; }
+    
+}
