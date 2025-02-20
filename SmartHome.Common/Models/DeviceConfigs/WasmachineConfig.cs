05db6011bd5f99db78fc0f1d15b2129674eb3048
Rik Dijkslag <46691159+rikdijkslag@users.noreply.github.com>
2025-02-20 11:56:02 +0100

    Telvisie en wasmachine config werkend gemaakt

--- SmartHome.Common/Models/DeviceConfigs/WasmachineConfig.cs
+++ SmartHome.Common/Models/DeviceConfigs/WasmachineConfig.cs
@@ -9,7 +9,8 @@ namespace SmartHome.Common.Models.Configs
     public class WasmachineConfig
     {
         public bool Ingeschakeld { get; set; }
-
         public string Programma { get; set; }
+        public DateTime ProgrammaStart { get; set; }
+        public int ProgrammaDuur { get; set; }
     }
 }
