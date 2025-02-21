28cf205f3107262e5eacbe373561e254870e1606
Rik Dijkslag <rikdijkslag@gmail.com>
2025-02-21 15:21:03 +0100

    Logs

---
+++ SmartHome.Backend/Api/LogService.cs
@@ -0,0 +1,20 @@
+﻿using SmartHome.Common.Api;
+
+namespace SmartHome.Backend.Api
+{
+    public class LogService : ILogService
+    {
+        private readonly ApiContext _ctx;
+
+        public LogService(ApiContext context)
+        {
+            _ctx = context;
+        }
+        public async Task<SuccessResponse> GetAllLogs(EmptySmartHomeRequest request)
+        {
+
+
+            return SuccessResponse.Success();
+        }
+    }
+}
---
+++ SmartHome.Common/Api/IlogService.cs
@@ -0,0 +1,21 @@
+﻿using SmartHome.Common.Models;
+using SmartHome.Common.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Common.Api
+{
+    public record LogListResponse(List<Log> Logs) : Response<LogListResponse>;
+
+    public interface ILogService
+    {
+        Task<LogListResponse> GetAllLogs(EmptySmartHomeRequest request);
+
+        public record CreateLogRequest(string Action, string Type) : SmartHomeRequest;
+        Task<SuccessResponse> CreateLog(CreateLogRequest request);
+    }
+
+}
